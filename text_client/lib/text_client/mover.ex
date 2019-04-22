ceaa2cbd74dde2b41f200d71cfeaa70c4b494dfa
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2019-04-22 22:12:31 +0200

    complete more stuffs

--- text_client/lib/text_client/mover.ex
+++ text_client/lib/text_client/mover.ex
@@ -2,13 +2,14 @@ defmodule TextClient.Mover do
   alias TextClient.State
 
   def make_move(game) do
-    new_game = Hangman.make_move(game.game_service, game.guess)
+    Hangman.make_move(game.game_service, game.guess)
+    pid = game.game_service
 
     %State{
-      game_service: new_game,
-      tally: Hangman.tally(new_game),
+      game_service: pid,
+      tally: Hangman.tally(pid),
       guess: "",
-      word: Hangman.reveal_word(new_game)
+      word: Hangman.reveal_word(pid)
     }
   end
 end
